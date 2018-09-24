cdc22c497944b76dc99307f091a51ada40283cf2
Ferdinand Malcher <mail@ferdinand-malcher.de>
2018-09-24 14:29:11 +0200

    Dashboard mit string[]

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'book-rating';
+  title = 'Book Rating!';
+
+  /*constructor() {
+    setTimeout(() => this.title = 'Hallo Jena', 2000);
+  }*/
 }
