6b874a9e7222e8bbe9efafb3aec6f6f4591b3604
Ferdinand Malcher <mail@ferdinand-malcher.de>
2018-09-25 13:19:38 +0200

    BookComponent: Service verwenden (noch nicht optimal)

--- book-rating/src/app/books/book/book.component.ts
+++ book-rating/src/app/books/book/book.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Book } from '../shared/book';
+import { BookRatingService } from '../shared/book-rating.service';
 
 @Component({
   selector: 'br-book',
@@ -10,13 +11,26 @@ export class BookComponent implements OnInit {
 
   @Input() book: Book;
 
-  constructor() { }
+  constructor(private rs: BookRatingService) { }
+
+  /* Langschreibweise (aber lieber TypeScript-Shorthand nutzen)
+  private rs: BookRatingService;
+  constructor(rs: BookRatingService) {
+    this.rs = rs;
+  } */
 
   ngOnInit() {
   }
 
   getStars(): any[] {
     return new Array(this.book.rating);
+  }
+
+  rateUp() {
+    const ratedBook = this.rs.rateUp(this.book);
+    console.log(ratedBook);
+
+    this.book = ratedBook; // noch nicht gut, weil das Dashboard nicht informiert wird
   }
 
 }
