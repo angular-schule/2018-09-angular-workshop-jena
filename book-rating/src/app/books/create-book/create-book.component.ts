91fc12bf80e24d6b49f555b328ee8f1732827dd9
Ferdinand Malcher <mail@ferdinand-malcher.de>
2018-09-26 11:12:29 +0200

    Reactive Forms: Validierung, Feedback, Abschicken

--- book-rating/src/app/books/create-book/create-book.component.ts
+++ book-rating/src/app/books/create-book/create-book.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'br-create-book',
@@ -14,10 +15,36 @@ export class CreateBookComponent implements OnInit {
 
   ngOnInit() {
     this.bookForm = new FormGroup({
-      isbn: new FormControl(''),
-      title: new FormControl(''),
+      isbn: new FormControl('', [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(13)
+      ]),
+      title: new FormControl('', Validators.required),
       description: new FormControl('')
     });
+  }
+
+  submitForm() {
+    // Daten holen und Buch erzeugen
+    const newBook: Book = {
+      ...this.bookForm.value,
+      rating: 1
+    };
+
+    console.log(newBook);
+
+  }
+
+
+  isInvalid(controlName: string): boolean {
+    const control = this.bookForm.get(controlName);
+    return control.invalid && control.dirty;
+  }
+  
+  hasError(controlName: string, errorCode: string): boolean {
+    const control = this.bookForm.get(controlName);
+    return control.hasError(errorCode) && control.dirty;
   }
 
 }
