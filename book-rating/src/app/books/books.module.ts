fd6ea15362949c89a6cdb2fe5797a20d792e75dd
Ferdinand Malcher <mail@ferdinand-malcher.de>
2018-09-26 09:30:39 +0200

    CreateBook mit TDF

--- book-rating/src/app/books/books.module.ts
+++ book-rating/src/app/books/books.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { BooksRoutingModule } from './books-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookComponent } from './book/book.component';
 import { GeneralSharedModule } from '../general-shared/general-shared.module';
+import { CreateBookComponent } from './create-book/create-book.component';
 
 @NgModule({
   imports: [
     CommonModule,
     BooksRoutingModule,
-    GeneralSharedModule
+    GeneralSharedModule,
+    FormsModule
   ],
   declarations: [
     DashboardComponent,
-    BookComponent
+    BookComponent,
+    CreateBookComponent
   ],
   exports: [DashboardComponent] // kann später wieder weg
 })
---
+++ book-rating/src/app/books/create-book/create-book.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'br-create-book',
+  templateUrl: './create-book.component.html',
+  styleUrls: ['./create-book.component.scss']
+})
+export class CreateBookComponent implements OnInit {
+
+  foo = 'test';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
