cc6c39afb149b3e16086287e76f1cda1d3d321af
Ferdinand Malcher <mail@ferdinand-malcher.de>
2018-09-27 09:59:48 +0200

    Experimente mit Observables

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { timer, Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'br-root',
@@ -8,7 +10,45 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Book Rating!';
 
-  /*constructor() {
-    setTimeout(() => this.title = 'Hallo Jena', 2000);
-  }*/
+  constructor() {
+
+    const myObs = timer(0, 1000).pipe(take(5));
+    // import { timer } from 'rxjs';
+    // import { take } from 'rxjs/operators';
+
+    const sub = myObs.subscribe(
+      e => console.log(e),
+      err => console.error(err),
+      () => console.log('COMPLETE')
+    );
+
+    setTimeout(() => {
+      sub.unsubscribe();
+    }, 3000);
+
+
+    const observer = {
+      next: e => console.log(e),
+      error: err => console.error(err),
+      complete: () => console.log('COMPLETE')
+    };
+
+
+    // Funktion: Producer Function
+    const myObs2 = new Observable((observer1) => {
+      observer1.next(3);
+      observer1.next(4);
+
+      setTimeout(() => {
+        observer1.next(5);
+        observer1.complete();
+
+        // kommt nicht durch, weil nach complete!
+        observer1.next(6);
+      }, 2000);
+    });
+
+    myObs2.subscribe(observer);
+
+  }
 }
